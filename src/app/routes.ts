ec5a691a5cfc404ba4eda156a39a5b74f6c91391
DESKTOP-HV4CS2N\Anne Lin <anne.lin.www@gmail.com>
2024-06-07 18:00:27 +0800

    modify: define two routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,5 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
-const routeConfig: Routes = [];
+const routeConfig: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    title: 'Home details',
+  },
+];
 
 export default routeConfig;
